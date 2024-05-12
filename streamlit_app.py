7c6fcf4dbb7da553e8e0e5c560e4cf74591bd9d8
TalalSaeed92 <talalsaeed970@gmail.com>
2024-05-12 14:03:44 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,50 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from joblib import load
+import dill
 
-"""
-# Welcome to Streamlit!
+# Load the pretrained model
+with open('pipeline.pkl', 'rb') as file:
+    model = dill.load(file)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+my_feature_dict = load('my_feature_dict.pkl')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Function to predict churn
+def predict_churn(data):
+    prediction = model.predict(data)
+    return prediction
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+st.title('Employee Churn Prediction App')
+st.subheader('Based on Employee Dataset')
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Display categorical features
+st.subheader('Categorical Features')
+categorical_input = my_feature_dict.get('CATEGORICAL')
+categorical_input_vals = {}
+for i, col in enumerate(categorical_input.get('Column Name').values()):
+    categorical_input_vals[col] = st.selectbox(col, categorical_input.get('Members')[i])
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Load numerical features
+numerical_input = my_feature_dict.get('NUMERICAL')
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# Display numerical features
+st.subheader('Numerical Features')
+numerical_input = my_feature_dict.get('NUMERICAL')
+numerical_input_vals = {}
+for col in numerical_input.get('Column Name'):
+    numerical_input_vals[col] = st.number_input(col)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Combine numerical and categorical input dicts
+input_data = dict(list(categorical_input_vals.items()) + list(numerical_input_vals.items()))
+
+input_data= pd.DataFrame.from_dict(input_data, orient='index').T
+
+# Churn Prediction
+if st.button('Predict'):
+    prediction = predict_churn(input_data)[0]
+    translation_dict = {'Yes':'Expected','No':'Not Expected'}
+    prediction_translate = translation_dict.get(prediction)
+    st.write(f'The Prediction is **{prediction}**, Hence Employee is **{prediction_translate}** to churn.')
+
+st.subheader('Created by Muhammad Talal Saeed')
+
